9309ba3cb9228e2435ece5611c3c43a27ac9f33d
tomherden@gmail.com <tomherden@gmail.com>
2024-08-15 21:08:21 +0000

    Edited GetDistrictCoordinates

--- server_code/GetDistrictCoordinates.py
+++ server_code/GetDistrictCoordinates.py
@@ -1,6 +1,4 @@
 import requests
-# import anvil.files
-# from anvil.files import data_files
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -10,29 +8,43 @@ import anvil.server
 # rather than in the user's browser.
 
 @anvil.server.callable
-def get_one_coordinate_pair(district_record):
+def get_districts():
+  # Get a list of entries from the Data Table, sorted by 'created' column, in descending order
+  return app_tables.districts.search()
+
+
+@anvil.server.callable
+def get_coordinate_pair_from_census_bureau(district_record):
+  STATE = "NY"
   # get data from record
   street = district_record["StreetLocation"]
   city = district_record["City"]
-  state = "NY"
   zip = district_record["Zip"]
 
   # construct URL for Census Bureau query
   website = "https://geocoding.geo.census.gov/geocoder/locations/"
-  address = f"{street}+{city},+{state}+{zip}"
+  address = f"{street}+{city},+{STATE}+{zip}"
   parameters = f"onelineaddress?address={address}&benchmark=4&format=json"
   URL = f"{website}{parameters}"
 
   # query Census Bureau
   response_dict = requests.get(URL).json()
+  print(response_dict)
 
   # extract and return coordinate pair
-  longitude = response_dict['result']['addressMatches'][0]['coordinates']['x']
-  latitude = response_dict['result']['addressMatches'][0]['coordinates']['y']
-  return longitude, latitude
-  
+  if response_dict["result"]["addressMatches"]:
+    longitude = round(response_dict["result"]["addressMatches"][0]["coordinates"]["x"], 4)
+    latitude = round(response_dict["result"]["addressMatches"][0]["coordinates"]["y"], 4)
+    return longitude, latitude
+  else:
+    anvil.alert(f"Census Bureau returned no coordinates for {address}")
+    return False
+
+
 @anvil.server.callable
-def refresh_all_coordinates():
-  
-  pass
-  
+def update_all_coordinates():
+  for district_row in get_districts():
+    coordinates = get_coordinate_pair_from_census_bureau(district_row)
+    if coordinates:
+      district_row["Longitude"], district_row["Latitude"] = coordinates[0], coordinates[1]
+ 
