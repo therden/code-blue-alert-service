f38852f3071386946f3ac7248c50f4a97d852ab0
tomherden@gmail.com <tomherden@gmail.com>
2024-08-19 19:34:17 +0000

    Edited Forecasts and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -12,3 +12,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # self.image_1.source = anvil.server.call('getForecastGraph')
+    self.image_1.source = anvil.server.call('test_plot')
--- server_code/Forecasts.py
+++ server_code/Forecasts.py
@@ -13,73 +13,86 @@ import anvil.server
 import anvil.mpl_util
 import numpy as np
 
-latitude, longitude = (42.4395, -76.5022)
-days = 4
-tempAdjustment = -25  # used to test windchill calc with summertime data...
-hours = 24 * days
-lastPeriodEligible = False
+# latitude, longitude = (42.4395, -76.5022)
+# days = 4
+# tempAdjustment = -25  # used to test windchill calc with summertime data...
+# hours = 24 * days
+# lastPeriodEligible = False
 
-keyForecastData = [getOneHourForecastData(period) for period in periods[:hours]]
+# keyForecastData = [getOneHourForecastData(period) for period in periods[:hours]]
 
-def calculateWindchill(temperature=80, windspeed=0):
-    T, V = temperature, windspeed
-    windchill = 35.74 + (0.6215*T) - (35.75*(V*0.16)) + (0.4275*(T*(V*0.16)))
-    return round(windchill, 1)
+# def calculateWindchill(temperature=80, windspeed=0):
+#     T, V = temperature, windspeed
+#     windchill = 35.74 + (0.6215*T) - (35.75*(V*0.16)) + (0.4275*(T*(V*0.16)))
+#     return round(windchill, 1)
 
-def getAllForecastData(latitude, longitude):
-  forecastURL = f"https://api.weather.gov/points/{latitude},{longitude}"
-  hourlyForecastURL = requests.get(forecastURL).json()["properties"]["forecastHourly"]
-  hourlyForecastJSON = requests.get(hourlyForecastURL).json() 
-  periods = hourlyForecastJSON["properties"]["periods"]
-  return periods
+# def getAllForecastData(latitude, longitude):
+#   forecastURL = f"https://api.weather.gov/points/{latitude},{longitude}"
+#   hourlyForecastURL = requests.get(forecastURL).json()["properties"]["forecastHourly"]
+#   hourlyForecastJSON = requests.get(hourlyForecastURL).json() 
+#   periods = hourlyForecastJSON["properties"]["periods"]
+#   return periods
 
-def getOneHourForecastData(oneHourlyForecastDict):
-    global lastPeriodEligible
+# def getOneHourForecastData(oneHourlyForecastDict):
+#     global lastPeriodEligible
     
-    period = oneHourlyForecastDict
-    betterTemp = int(period['temperature']) + tempAdjustment
-    betterWindSpeed = int(period['windSpeed'].split()[0])
+#     period = oneHourlyForecastDict
+#     betterTemp = int(period['temperature']) + tempAdjustment
+#     betterWindSpeed = int(period['windSpeed'].split()[0])
     
-    newPeriod = dict()
-    newPeriod["startTime"] = datetime.strptime(period['startTime'], '%Y-%m-%dT%H:%M:%S%z')
-    newPeriod["temperatureF"] = betterTemp
-    newPeriod["windSpeedMPH"] = betterWindSpeed
-    newPeriod["windChill"] = calculateWindchill(betterTemp, betterWindSpeed)
+#     newPeriod = dict()
+#     newPeriod["startTime"] = datetime.strptime(period['startTime'], '%Y-%m-%dT%H:%M:%S%z')
+#     newPeriod["temperatureF"] = betterTemp
+#     newPeriod["windSpeedMPH"] = betterWindSpeed
+#     newPeriod["windChill"] = calculateWindchill(betterTemp, betterWindSpeed)
     
-    newPeriod["consecutive"] = False
-    if newPeriod["windChill"] <= 32:
-        if lastPeriodEligible:
-            newPeriod["consecutive"] = True
-        else:
-            lastPeriodEligible = True
-    else:
-        lastPeriodEligible = False
-    return newPeriod
+#     newPeriod["consecutive"] = False
+#     if newPeriod["windChill"] <= 32:
+#         if lastPeriodEligible:
+#             newPeriod["consecutive"] = True
+#         else:
+#             lastPeriodEligible = True
+#     else:
+#         lastPeriodEligible = False
+#     return newPeriod
   
-def getForecastGraph(keyForecastData, days):
-  graphData = {item['startTime']:item['windChill'] for item in keyForecastData}
-  minTemp = min(graphData.values())
-  maxTemp = max(graphData.values())
-  dateList = set(item['startTime'].strftime('%Y-%m-%d') for item in keyForecastData)
+# @anvil.server.callable
+# def getForecastGraph(keyForecastData, days):
+#   graphData = {item['startTime']:item['windChill'] for item in keyForecastData}
+#   minTemp = min(graphData.values())
+#   maxTemp = max(graphData.values())
+#   dateList = set(item['startTime'].strftime('%Y-%m-%d') for item in keyForecastData)
   
-  fig, ax = plt.subplots()
-  ax.plot(graphData.keys(), graphData.values(), color='darkgray')
-  ax.xaxis.set_major_formatter(ConciseDateFormatter(ax.xaxis.get_major_locator())) 
-  ax.xaxis.set_minor_locator(ticker.AutoMinorLocator(8))
-  plt.xlabel('Hours')
-  plt.ylabel('Fahrenheit')
-  plt.xlabel('Hourly Projections')
-  plt.title(f'Wind Chill Temperature: {days} day Forecast')
+#   fig, ax = plt.subplots()
+#   ax.plot(graphData.keys(), graphData.values(), color='darkgray')
+#   ax.xaxis.set_major_formatter(ConciseDateFormatter(ax.xaxis.get_major_locator())) 
+#   ax.xaxis.set_minor_locator(ticker.AutoMinorLocator(8))
+#   plt.xlabel('Hours')
+#   plt.ylabel('Fahrenheit')
+#   plt.xlabel('Hourly Projections')
+#   plt.title(f'Wind Chill Temperature: {days} day Forecast')
   
-  ax.vlines(x=list(dateList)[1:], ymin=minTemp, ymax=maxTemp, colors='lightgray', ls='-')
+#   ax.vlines(x=list(dateList)[1:], ymin=minTemp, ymax=maxTemp, colors='lightgray', ls='-')
   
-  if minTemp <= 32:
-      plt.axhline(y=32, color='red', linestyle='-')
-      coldDataPoints = {item['startTime']:item['windChill'] for item in keyForecastData if item['windChill'] <= 32}
-      xs, ys = list(coldDataPoints.keys()), list(coldDataPoints.values())
-      ax.plot(coldDataPoints.keys(), coldDataPoints.values(), color='cornflowerblue')
-      ax.fill_between(xs, ys, 32, color='cornflowerblue', interpolate=True)
+#   if minTemp <= 32:
+#       plt.axhline(y=32, color='red', linestyle='-')
+#       coldDataPoints = {item['startTime']:item['windChill'] for item in keyForecastData if item['windChill'] <= 32}
+#       xs, ys = list(coldDataPoints.keys()), list(coldDataPoints.values())
+#       ax.plot(coldDataPoints.keys(), coldDataPoints.values(), color='cornflowerblue')
+#       ax.fill_between(xs, ys, 32, color='cornflowerblue', interpolate=True)
     
-  # plt.show()
+#   # plt.show()
+#   return anvil.mpl_util.plot_image()
+
+@anvil.server.callable
+def test_plot():
+    # Make a nice wiggle
+  x = [1, 2, 3]
+  y = [7.3, 2.7, 5]
+  
+  # Plot it in the normal Matplotlib way
+  plt.figure(1, figsize=(30,15))
+  plt.plot(x, y, 'crimson')  
+  
+  # Return this plot as a PNG image in a Media object
   return anvil.mpl_util.plot_image()
-
