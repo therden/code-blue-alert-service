6dd4b3d585816a8dc7040d93e4329ce0efa8eede
tomherden@gmail.com <tomherden@gmail.com>
2024-08-30 21:36:46 +0000

    Created Routes; Edited Forecast

---
+++ server_code/Routes.py
@@ -0,0 +1,27 @@
+import anvil.files
+from anvil.files import data_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+@anvil.server.route("/hello")
+def hello(**p):
+  return {"hello":"world"}
+
+@anvil.server.route("/forecast/test")
+def forecast_test(**p):
+  return anvil.server.FormResponse("Forecast")
+  
