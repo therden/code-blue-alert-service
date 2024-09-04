16609da706b8738e94816d0be5646f3c3591c1bf
tomherden@gmail.com <tomherden@gmail.com>
2024-09-04 14:14:05 +0000

    Created LocationsLinks; Deleted listForm

--- client_code/LocationsLinks/__init__.py
+++ client_code/LocationsLinks/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import listFormTemplate
+from ._anvil_designer import LocationsLinksTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,7 +6,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class listForm(listFormTemplate):
+class LocationsLinks(LocationsLinksTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
