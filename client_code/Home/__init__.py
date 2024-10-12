4377138c3b56178eebc88e742ab36b83a966ed7d
tomherden@gmail.com <tomherden@gmail.com>
2024-10-12 22:17:37 +0000

    Created codeblueinfo_512x512.png and PlaceholderText; Edited settings and Home; Deleted Home_copy

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -4,16 +4,18 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..PlaceholderText import PlaceholderText
 
 
 class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.content_panel.add_component(PlaceholderText())
 
     # Any code you write here will run before the form opens.
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     # self.image_1.source = anvil.server.call('getForecastGraph')
-    self.image_1.source = anvil.server.call('test_plot')
+    self.image_1.source = anvil.server.call("test_plot")
