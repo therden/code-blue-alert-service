98ed1f296f81420fe1f86a014335e1fee615a112
tomherden@gmail.com <tomherden@gmail.com>
2024-10-13 18:04:59 +0000

    Fine-tune Location-specific Alert page text and styling

--- client_code/Forecast/__init__.py
+++ client_code/Forecast/__init__.py
@@ -16,58 +16,35 @@ class Forecast(ForecastTemplate):
     self.location = self.record["NormalizedName"]
     self.updateForm()
 
-  def set_rt_background(self):
-    if not self.record["CodeBlueQualified"]:
-      rt_background = "lemonchiffon"
-    else:
-      rt_background = "theme:Primary Container"
-    self.rt_headline.background = rt_background
-    self.rt_footer.background = rt_background
-
-  def build_headline(self):
-    if not self.record["CodeBlueQualified"]:
-      in_effect = "IS NOT"
-    else:
+  def set_alert_text_and_style(self):
+    if self.record["CodeBlueQualified"]:
       in_effect = "IS"
-    forecast_start = f'{self.record["NOAAupdate"]:%b %d}'
-    forecast_end = f'{(self.record["NOAAupdate"] + timedelta(days=1)):%b %d}'
-    # forecast_for = f"{forecast_start:%b %d} - {forecast_end:%b %d}"
-    noaa_forecast_datetime = self.record["NOAAupdate"]
-    forecast_time = f"{noaa_forecast_datetime:%I:%M %p}"
-    self.rt_headline.data = {
-      "in_effect": in_effect,
-      # "forecast_for_date": forecast_for,
-      "forecast_for_date": forecast_start,
-    }
+      self.rt_footer.content += f'\n### The Alert will remain in effect until **either** 7 AM on {(self.record["NOAAupdate"] + timedelta(days=1)):%b %d} **or** when Wind Chill temperatures exceed 32˚F -- whichever is *later*.'
+      rt_background = "theme:Primary Container"
+    else:
+      in_effect = "IS NOT"
+      rt_background = "lemonchiffon"
     self.rt_footer.data = {
       "forecast_datetime": f'{self.record["NOAAupdate"]:%b %d %I:%M %p}',
-      "forecast_until": forecast_end,
+      "until_date": f'{(self.record["NOAAupdate"] + timedelta(days=1)):%b %d}',
     }
-
-  # def set_and_style_alert(self):
-  #   if self.record["CodeBlueQualified"]:
-  #     in_effect = "IS"
-  #   else:
-  #     in_effect = "IS NOT"
-  #   top_text = f'## A Code Blue Alert **{in_effect}** in effect for the {forecast_for_date} overnight.'
+    self.rt_header.data = {
+      "in_effect": in_effect,
+      "for_date": f'{self.record["NOAAupdate"]:%b %d}',
+    }
+    self.rt_header.background = rt_background
+    self.rt_footer.background = rt_background
 
   def updateForm(self):
     location = self.location
     record = self.record
     self.label_1.text = f"codeblue.info/for/{location}"
-    self.set_rt_background()
-    self.build_headline()
     self.image_1.source = record["LastGraph"]
-    # lastDownload = record["DataRequested"].strftime("%Y-%m-%d %I:%M %p")
-    # lastNOAAupdate = record["NOAAupdate"].strftime("%I:%M %p")
-    # self.label_2.text = (
-    #   f"Updated {lastDownload} (from NOAA's {lastNOAAupdate} forecast.)"
-    # )
+    self.set_alert_text_and_style()
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     record = self.record
     anvil.server.call("updateForecast", record)
     anvil.server.call("updateForecastGraph", record)
-    # anvil.server.call("updateForecast", record)
     self.updateForm()
